0b3d4e1df5f1a5bbe5538e98503f6d25b112bc4e
Tab Nahida <tabye320@outlook.com>
2025-11-24 06:18:21 +0000

    change dependence installation way

--- bingsift/__init__.py
+++
@@ -1,9 +0,0 @@
-from .parser import parse_html, build_bing_url, extract_bing_click_target
-from .filters import filter_results
-
-__all__ = [
-    "parse_html",
-    "build_bing_url",
-    "filter_results",
-    "extract_bing_click_target",
-]
